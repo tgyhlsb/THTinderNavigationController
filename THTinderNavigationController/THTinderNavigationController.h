566cc9ce5b54272ac34130f97c206129207313d4
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-11-04 14:13:02 +0000

    new THTinderNavigationController

---
+++ THTinderNavigationController/THTinderNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  THTinderNavigationController.h
+//  THTinderNavigationControllerExample
+//
+//  Created by Tanguy Hélesbeux on 04/11/2014.
+//  Copyright (c) 2014 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THTinderNavigationController : UINavigationController
+
+@end
---
+++ THTinderNavigationController/THTinderNavigationController.m
@@ -0,0 +1,24 @@
+//
+//  THTinderNavigationController.m
+//  THTinderNavigationControllerExample
+//
+//  Created by Tanguy Hélesbeux on 04/11/2014.
+//  Copyright (c) 2014 Tanguy Hélesbeux. All rights reserved.
+//
+
+#import "THTinderNavigationController.h"
+
+@interface THTinderNavigationController ()
+
+@end
+
+@implementation THTinderNavigationController
+
+#pragma mark - View life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
